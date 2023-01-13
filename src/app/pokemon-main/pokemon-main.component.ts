aa45349a1e2db040b518fd2a90cbf1fd89c181d4
Sidney Henderson <sidneyvgcc8988@yahoo.com>
2023-01-13 16:30:08 -0500

    First commit

---
+++ src/app/pokemon-main/pokemon-main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-main',
+  templateUrl: './pokemon-main.component.html',
+  styleUrls: ['./pokemon-main.component.scss']
+})
+export class PokemonMainComponent {
+
+}
